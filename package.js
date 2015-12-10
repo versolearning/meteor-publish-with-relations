d6b079ca262b3e8963fd8fa8e976c7c0db2c5ed7
David Burles <dburles@gmail.com>
2015-12-10 14:37:34 +1100

    update package.js

--- package.js
+++ package.js
@@ -1,20 +1,23 @@
 Package.describe({
   name: "tmeasday:publish-with-relations",
   summary: "Publish associated collections at once.",
-  version: "0.2.0",
+  version: "0.2.1",
   git: "https://github.com/tmeasday/meteor-publish-with-relations.git"
 });
 
 Package.on_use(function(api) {
   api.versionsFrom("METEOR@0.9.0");
-  api.use('coffeescript', 'server');
+  api.use(['coffeescript', 'underscore'], 'server');
   api.add_files('publish_with_relations.coffee', 'server');
 });
 
 Package.on_test(function(api) {
-  api.use(['tinytest', 'coffeescript', "tmeasday:publish-with-relations", 'mongo-livedata'], 'server');
-  api.use(['tinytest'], 'client')
-  
+  api.use([
+    'tinytest',
+    'coffeescript',
+    'underscore',
+    'tmeasday:publish-with-relations',
+    'mongo-livedata'], 'server');
+
   api.add_files('publish_with_relations_test.coffee', 'server');
-  api.add_files('tests/client.js', 'client');
 });
